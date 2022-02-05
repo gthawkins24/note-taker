382e43a12f51f88d00d150e45a584de1d69aae92
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-02-05 09:54:04 -0500

    fixed start script

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const generateID = require('generate-unique-id');
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
+const { notes } = require("./db/db");
 
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
@@ -14,10 +15,29 @@ const newNote = (body, notes) => {
     notes.push(note);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notes })
+        JSON.stringify({ notes: notes }, null, 2)
     );
+    return note;
 };
 
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}.`);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = generateID();
+    const note = newNote(req.body, notes);
+    res.json(note);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}.`);
 });
